1b8e2313a2c072081a44fc86c53897538d004de4
Grzegorz Błaszczyk <grzegorz.blaszczyk@goodylabs.com>
2019-08-16 12:00:11 +0200

    ADD: working create_buy_order() and create_sell_order() with code deduplication

--- lib/degiro_client/create_order.rb
+++ lib/degiro_client/create_order.rb
@@ -2,7 +2,7 @@ require 'json'
 
 module DeGiro
   class CreateOrder
-    BUY_SELL    = { buy: 0, sell: 1 }.freeze
+    BUY_SELL    = { buy: "BUY", sell: "SELL" }.freeze
     ORDER_TYPES = { limited: 0, stop_limited: 1, market_order: 2, stop_loss: 3 }.freeze
     TIME_TYPES  = { day: 1, permanent: 3 }.freeze
 
@@ -11,27 +11,27 @@ module DeGiro
     end
 
     def create_buy_order(product_id:, size:, price:)
-      order = order(BUY_SELL[:buy], product_id, size, price)
-      binding.pry
-      response = check_order(order)
-      binding.pry
-      confirmation_id = JSON.parse(response.body)['confirmationId']
-      confirm_order(order, confirmation_id)
+      create_order_with_confirmation(BUY_SELL[:buy], product_id, size, price)
     end
 
     def create_sell_order(product_id:, size:, price:)
-      order = order(BUY_SELL[:sell], product_id, size, price)
-      confirmation_id = JSON.parse(check_order(order).body)['confirmationId']
-      confirm_order(order, confirmation_id)
+      create_order_with_confirmation(BUY_SELL[:sell], product_id, size, price)
     end
 
     private
 
+    def create_order_with_confirmation(buy_sell:, product_id:, size:, price:)
+      order = order(buy_sell, product_id, size, price)
+      response = check_order(order)
+      confirmation_id = JSON.parse(response.body)['data']['confirmationId']
+      confirm_order(order, confirmation_id)
+    end
+
     def order(type, product_id, size, price)
       {
-        buysell:   type,
+        buySell:   type,
         orderType: ORDER_TYPES[:limited],
-        productId: product_id,
+        productId: product_id.to_i,
         size:      size,
         timeType:  TIME_TYPES[:permanent],
         price:     price
@@ -39,7 +39,6 @@ module DeGiro
     end
 
     def check_order(order)
-      binding.pry
       @connection.post(check_order_url) do |req|
         req.headers['Content-Type'] = 'application/json; charset=UTF-8'
         req.body = order.to_json
